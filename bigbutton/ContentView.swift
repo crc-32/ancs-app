7c3b59d5eea94f70d80fbaf36eeaa1785b9c62a8
crc-32 <me@crc32.dev>
2021-08-06 00:11:09 +0100

    Initial Commit

---
+++ bigbutton/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bigbutton
+//
+//  Created by crc32 on 06/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bigbutton/bigbuttonApp.swift
@@ -0,0 +1,17 @@
+//
+//  bigbuttonApp.swift
+//  bigbutton
+//
+//  Created by crc32 on 06/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct bigbuttonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
